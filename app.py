b12d2575425c2ac1a7ac4cc9c736b0012f37951a
Slawomir Grzebyk <grzebyk.slawomir@gmail.com>
2024-03-27 09:17:32 +0000

    change pkl model

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import pickle
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 # app.config['ENV'] = 'development'
 # app.config['DEBUG'] = True
 
